94db879d49ca14ec412f572e22167765bee88977
Hasibul Hasan <39868483+hhasan9979@users.noreply.github.com>
2025-02-13 22:46:49 -0500

    Update main.js

--- main.js
+++ main.js
@@ -222,7 +222,7 @@ const animationTimeline = () => {
       {
         scale: 1,
         rotationY: 0,
-        color: "#ff69b4",
+        color: "#fc4444",
         ease: Expo.easeOut,
       },
       0.1,
